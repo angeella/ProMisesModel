1b92cd39b14d00b9609f12bda8dec33715b101cc
Angela Andreella <36375295+angeella@users.noreply.github.com>
2019-11-28 13:04:10 +0100

    Update priorGPA
    Add Parallelization

--- priorGPA.py
+++ priorGPA.py
@@ -19,10 +19,35 @@ import decimal as dc
 #context = getcontext()
 #context.prec = 55
 import math
+import multiprocessing as mp
 
 __all__= ['priorGPA'] #explicitly exports the symbols priorHyA
 
-#if you need explit here some function as 
+#Main function that will be Parallelize
+
+def gpaSub(X, Q, k, ref_ds, col, scaling, reflection):
+
+    if Q is None:
+        Q = np.zeros((col,col)) 
+    #Put transposes to save memory.
+    U, s, Vt =  np.linalg.svd((ref_ds.T.dot(X) + k * Q.T).T, full_matrices = False)    
+          
+    if not reflection: 
+        s_new = np.diag(np.ones(len(s)))
+        s_new[-1,-1] = np.sign(np.linalg.det(U.dot(Vt)))
+        Tr = U.dot(s_new).dot(Vt)
+        scale = np.sum(s_new * s)
+    else:
+        Tr = U.dot(Vt)
+        scale = np.sum(s)
+                
+    R = Tr
+
+    if not scaling:
+        Xest = X.dot(R)
+    else:
+        Xest = X.dot(R)* scale
+    return Xest, R
 
 class priorGPA(ClassWithCollections):
     
@@ -144,39 +169,15 @@ class priorGPA(ClassWithCollections):
             R = []
             #ref_start = ref_ds
             #del ref_ds
-            for i in range(ndatasets):
-                #M = ref_ds
-                #Xs = M.T + k * Q 
-                if not subj:
-                    if Q is None:
-                        Q = np.zeros((col,col)) 
-                    #Put transposes to save memory.
-                    U, s, Vt =  np.linalg.svd((ref_ds.T.dot(X[i]) + k * Q.T).T, full_matrices = False)
-                    #U, s, Vt =  np.linalg.svd(X[i].T.dot(ref_ds) + k * Q, full_matrices = False)
-
-                else:
-                    if Q is None:
-                        Q = np.zeros(col) 
-                    U, s, Vt =  np.linalg.svd((ref_ds.T.dot(X[i]) + k * Q[i].T).T, full_matrices = False)      
-                    #U, s, Vt =  np.linalg.svd(X[i].T.dot(ref_ds) + k * Q[i], full_matrices = False)                
-
-                if not reflection: 
-                    s_new = np.diag(np.ones(len(s)))
-                    s_new[-1,-1] = np.sign(np.linalg.det(U.dot(Vt)))
-                    Tr = U.dot(s_new).dot(Vt)
-                    scale = np.sum(s_new * s)
-                else:
-                    Tr = U.dot(Vt)
-                    scale = np.sum(s)
-                
-                R.append(Tr)
-                #R[i] = U.dot(Vh)
-                if not scaling:
-                    Xest.append(X[i].dot(R[i]))
-                else:
-                    Xest.append((X[i].dot(R[i]))* scale) 
-                #Xest[i] = Xest[i].dot(R[i])
+            pool = mp.Pool(mp.cpu_count())
+            if subj:
+                out = [pool.apply(gpaSub, args=(x, q, k, ref_ds, col, scaling, reflection)) for x,q in zip(X,Q)]
+            else:
+                out = [pool.apply(gpaSub, args=(x, Q, k, ref_ds, col, scaling, reflection)) for x in X]
+            pool.close()  
             count +=1
+            Xest = [x[0] for x in out]
+            R = [x[1] for x in out]
             ref_ds_old = np.copy(ref_ds)
             #print(ref_ds_old)
             ref_ds = np.mean(Xest, axis=0)
